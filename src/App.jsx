83c7ab79ced6bb67dad9e1778b1299ac45baaac9
albertomarti <146454052+albertomartii@users.noreply.github.com>
2026-04-21 18:24:22 +0200

    cambios de diseño y animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,43 @@ export default function App() {
   return (
     <div className="app-container">
       <div className="top-row">
-        <LiquidGlass id="glass1">Alberto Martí Llorens</LiquidGlass>
-        <LiquidGlass id="glass2">IES El Caminàs</LiquidGlass>
-        <LiquidGlass id="glass3">DAW</LiquidGlass>
+
+        <LiquidGlass id="glass1">
+          <h1
+            className="chromatic fisheye wobble"
+            data-text="Alberto Martí Llorens"
+          >
+            Alberto Martí Llorens
+          </h1>
+        </LiquidGlass>
+
+        <LiquidGlass id="glass2">
+          <h2
+            className="chromatic fisheye wobble"
+            data-text="IES El Caminàs"
+          >
+            IES El Caminàs
+          </h2>
+        </LiquidGlass>
+
+        <LiquidGlass id="glass3">
+          <h2
+            className="chromatic fisheye wobble"
+            data-text="DAW"
+          >
+            DAW
+          </h2>
+        </LiquidGlass>
+
       </div>
 
       <LiquidGlass id="glass4">
-        Lenguajes de programación:
+        <h2
+          className="chromatic fisheye wobble"
+          data-text="Lenguajes de programación"
+        >
+          Lenguajes de programación
+        </h2>
       </LiquidGlass>
     </div>
   );
